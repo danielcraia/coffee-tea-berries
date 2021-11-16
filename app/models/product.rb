179ff172096b500ed46009d87340ef806bc2c5ae
daniel <daniel.craia@sparktech.ro>
2021-11-16 23:38:46 +0200

    Typo: inherit ApplicationRecord from Product class

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Product < ActiveRecord
+class Product < ApplicationRecord
   has_many :cart_products, dependent: :destroy
   has_many :carts, through: :cart_products
 
