235333927939bf57ad6145e5f3ac7cce10b94117
daniel <daniel.craia@sparktech.ro>
2021-11-18 00:24:31 +0200

    Add update and destroy endpoints for cart_products_controller

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -1,14 +1,29 @@
 # frozen_string_literal: true
 
 class CartProductsController < ApplicationController
-  before_action :initialize_cart_product
+  before_action :initialize_cart_product, only: :create
+  before_action :load_cart_product, only: %w[destroy update]
 
   def create
     @cart_product.quantity += create_params[:quantity].to_i
 
-    flash.now[:messages] = cart.errors.full_messages unless @cart_product.save
+    flash.now[:messages] = @cart_product.errors.full_messages unless @cart_product.save
 
     redirect_to edit_cart_url(create_params[:cart_id])
+  end
+
+  def update
+    flash.now[:messages] = @cart_product.errors.full_messages unless @cart_product.update(
+      quantity: update_params[:quantity]
+    )
+
+    redirect_to edit_cart_url(@cart_product.cart_id)
+  end
+
+  def destroy
+    flash.now[:messages] = "Product could not be removed!" unless @cart_product.destroy
+
+    redirect_to edit_cart_url(@cart_product.cart_id)
   end
 
   private
@@ -17,7 +32,15 @@ class CartProductsController < ApplicationController
     @cart_product = CartProduct.find_or_initialize_by(create_params.slice(:cart_id, :product_id))
   end
 
+  def load_cart_product
+    @cart_product = CartProduct.find(params[:id])
+  end
+
   def create_params
     params.require(:cart_product).permit(:cart_id, :product_id, :quantity)
+  end
+
+  def update_params
+    params.require(:cart_product).permit(:quantity)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :carts
   resources :products, only: :index
-  resources :cart_products, only: :create
+  resources :cart_products, only: %w[create update destroy]
 
   root to: "carts#index"
 end
